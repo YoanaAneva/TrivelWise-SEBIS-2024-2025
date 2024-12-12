bf065df8b9837620a6c5d87953c642ae55fb3d63
Yoana.Aneva <ioana.aneva@gmail.com>
2024-12-12 17:48:25 +0200

    Create Offer Repository

--- TravelWise-backend/src/main/java/com/example/travelwise/repository/OfferRepository.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/repository/OfferRepository.java
@@ -1,9 +1,14 @@
 package com.example.travelwise.repository;
 
 import com.example.travelwise.entity.Offer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OfferRepository extends JpaRepository<Offer, Long> {
+    Page<Offer> findAllByCategoryId(Long categoryId, Pageable pageable);
+    Page<Offer> findAllByCategoryDepartmentId(Long departmentId, Pageable pageable);
+
 }
