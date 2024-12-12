67efd836c7f545dba45f46f26dfe8421374af749
Yoana.Aneva <ioana.aneva@gmail.com>
2024-12-12 17:46:00 +0200

    Create offer mapper

--- TravelWise-backend/src/main/java/com/example/travelwise/mapper/OfferMapper.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/mapper/OfferMapper.java
@@ -1,2 +1,29 @@
-package com.example.travelwise.mapper;public interface OfferMapper {
+package com.example.travelwise.mapper;
+
+import com.example.travelwise.dto.OfferDTO;
+import com.example.travelwise.entity.Image;
+import com.example.travelwise.entity.Offer;
+import org.mapstruct.IterableMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface OfferMapper {
+    @Mapping(source = "category.id", target = "categoryId")
+    @Mapping(source = "images", target = "imageUrls")
+    OfferDTO mapToDTO(Offer offer);
+
+    @Mapping(source = "categoryId", target = "category.id")
+    Offer mapToEntity(OfferDTO offerDTO);
+
+    @IterableMapping(qualifiedByName = "mapImageToUrl")
+    List<String> mapImagesToUrls(List<Image> images);
+
+    @Named("mapImageToUrl")
+    default String mapImageToUrl(Image image) {
+        return image != null ? image.getUrl() : null;
+    }
 }
