9fbd9820dff78c9613acea10f4ee67dcf4108a4f
Yoana.Aneva <ioana.aneva@gmail.com>
2024-12-13 23:38:41 +0200

    Add cart repository

--- TravelWise-backend/src/main/java/com/example/travelwise/repository/CartRepository.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/repository/CartRepository.java
@@ -1,2 +1,9 @@
-package com.example.travelwise.repository;public interface CartRepository {
+package com.example.travelwise.repository;
+
+import com.example.travelwise.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
 }
