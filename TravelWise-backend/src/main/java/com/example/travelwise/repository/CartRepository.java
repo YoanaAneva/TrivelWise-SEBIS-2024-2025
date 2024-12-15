52c33a5e516f0e568f0fa45d6839b564a6969ec1
Yoana.Aneva <ioana.aneva@gmail.com>
2024-12-15 13:14:25 +0200

    Add delete by user id

--- TravelWise-backend/src/main/java/com/example/travelwise/repository/CartRepository.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/repository/CartRepository.java
@@ -4,6 +4,10 @@ import com.example.travelwise.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUserId(Long userId);
+    void DeleteByUserId(Long userId);
 }
