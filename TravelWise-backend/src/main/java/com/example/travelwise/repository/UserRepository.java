f5f11c92e67f0750e0e84bf7bba95eb5d2ecd36a
Yoana.Aneva <ioana.aneva@gmail.com>
2024-12-15 13:13:20 +0200

    Remove unused imports

--- TravelWise-backend/src/main/java/com/example/travelwise/repository/UserRepository.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/repository/UserRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Page<User> findAll(Pageable pageable);
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
