8a872fffd204ded68ac1cfc6b8840cc8379ae86e
Yoana.Aneva <ioana.aneva@gmail.com>
2024-12-15 13:10:37 +0200

    Add more validation fields

--- TravelWise-backend/src/main/java/com/example/travelwise/dto/CategoryDTO.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/dto/CategoryDTO.java
@@ -1,13 +1,16 @@
 package com.example.travelwise.dto;
 
-import com.example.travelwise.entity.Department;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CategoryDTO {
     private Long id;
+    @NotBlank(message = "Category name cannot be blank")
     private String name;
     private String description;
     private String icon;
+    @NotNull(message = "Department id cannot be null")
     private Long departmentId;
 }
--- TravelWise-backend/src/main/java/com/example/travelwise/dto/UserDTO.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/dto/UserDTO.java
@@ -1,7 +1,6 @@
 package com.example.travelwise.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
