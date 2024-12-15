7c7636f453679df1eb1bd3f7b30855600f5c9c09
Yoana.Aneva <ioana.aneva@gmail.com>
2024-12-15 13:07:18 +0200

    Add non pageable method for getting reservations by cart

--- TravelWise-backend/src/main/java/com/example/travelwise/repository/ReservationRepository.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/repository/ReservationRepository.java
@@ -1,2 +1,16 @@
-package com.example.travelwise.repository;public interface ReservationRepository {
+package com.example.travelwise.repository;
+
+import com.example.travelwise.entity.Reservation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByCartId(Long cartId);
+    Page<Reservation> findByCartId(Long cartId, Pageable pageable);
+    Page<Reservation> findByCartUserId(Long userId, Pageable pageable);
 }
