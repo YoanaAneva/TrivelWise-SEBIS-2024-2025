47597fe2c1ef4d4cef77e88666e4d4931b6a8614
Yoana.Aneva <ioana.aneva@gmail.com>
2024-12-16 13:53:22 +0200

    Create mapper

--- TravelWise-backend/src/main/java/com/example/travelwise/mapper/CategoryMapper.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/mapper/CategoryMapper.java
@@ -1,2 +1,15 @@
-package com.example.travelwise.mapper;public interface CategoryMapper {
+package com.example.travelwise.mapper;
+
+import com.example.travelwise.dto.CategoryDTO;
+import com.example.travelwise.entity.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    @Mapping(source = "department.id", target = "departmentId")
+    CategoryDTO mapToDTO(Category category);
+
+    @Mapping(source = "departmentId", target = "department.id")
+    Category mapToEntity(CategoryDTO categoryDTO);
 }
--- TravelWise-backend/src/main/java/com/example/travelwise/mapper/DepartmentMapper.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/mapper/DepartmentMapper.java
@@ -1,2 +1,11 @@
-package com.example.travelwise.mapper;public interface DepartmentMapper {
+package com.example.travelwise.mapper;
+
+import com.example.travelwise.dto.DepartmentDTO;
+import com.example.travelwise.entity.Department;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface DepartmentMapper {
+    DepartmentDTO mapToDTO(Department department);
+    Department mapToEntity(DepartmentDTO departmentDTO);
 }
