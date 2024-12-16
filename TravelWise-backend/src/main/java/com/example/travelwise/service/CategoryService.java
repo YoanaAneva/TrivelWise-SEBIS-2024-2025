2ef428603300f601baa34f1de0a2d4e9c2878bf5
Yoana.Aneva <ioana.aneva@gmail.com>
2024-12-16 13:56:47 +0200

    Create service

--- TravelWise-backend/src/main/java/com/example/travelwise/service/CategoryService.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/service/CategoryService.java
@@ -1,2 +1,32 @@
-package com.example.travelwise.service;public class CategoryService {
+package com.example.travelwise.service;
+
+import com.example.travelwise.dto.CategoryDTO;
+import com.example.travelwise.entity.Category;
+import com.example.travelwise.mapper.CategoryMapper;
+import com.example.travelwise.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository, CategoryMapper categoryMapper) {
+        this.categoryRepository = categoryRepository;
+        this.categoryMapper = categoryMapper;
+    }
+
+    public List<CategoryDTO> getAllCategories() {
+        return categoryRepository.findAll().stream().map(categoryMapper::mapToDTO).toList();
+    }
+
+    public List<CategoryDTO> getCategoriesByDepartment(Long departmentId) {
+        return categoryRepository.findByDepartmentId(departmentId).stream()
+                .map(categoryMapper::mapToDTO)
+                .toList();
+    }
 }
--- TravelWise-backend/src/main/java/com/example/travelwise/service/DepartmentService.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/service/DepartmentService.java
@@ -1,2 +1,25 @@
-package com.example.travelwise.service;public class DepartmentService {
+package com.example.travelwise.service;
+
+import com.example.travelwise.dto.DepartmentDTO;
+import com.example.travelwise.mapper.DepartmentMapper;
+import com.example.travelwise.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentService {
+    private final DepartmentRepository departmentRepository;
+    private final DepartmentMapper departmentMapper;
+
+    @Autowired
+    public DepartmentService(DepartmentRepository departmentRepository, DepartmentMapper departmentMapper) {
+        this.departmentRepository = departmentRepository;
+        this.departmentMapper = departmentMapper;
+    }
+
+    public List<DepartmentDTO> getAllDepartments() {
+        return departmentRepository.findAll().stream().map(departmentMapper::mapToDTO).toList();
+    }
 }
