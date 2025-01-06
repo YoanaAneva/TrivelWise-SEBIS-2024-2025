79bdadc1d7927372351296072a76abed8d8240f0
Yoana.Aneva <ioana.aneva@gmail.com>
2025-01-06 11:35:29 +0200

    Use stored procedure from data base

--- TravelWise-backend/src/main/java/com/example/travelwise/repository/CartRepository.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/repository/CartRepository.java
@@ -2,11 +2,20 @@ package com.example.travelwise.repository;
 
 import com.example.travelwise.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
     Optional<Cart> findByUserId(Long userId);
+
+    @Transactional
+    @Modifying
+    @Query(value = "CALL process_cart_payment(:cartId)", nativeQuery = true)
+    void processCartPayment(@Param("cartId") Long cartId);
 }
