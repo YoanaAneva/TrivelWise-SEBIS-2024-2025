d419165ea8e8006a441294bcc6846a1722c4b203
Yoana.Aneva <ioana.aneva@gmail.com>
2025-01-21 00:05:02 +0200

    Add logic for deleting department

--- TravelWise-backend/src/main/java/com/example/travelwise/controller/DepartmentController.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/controller/DepartmentController.java
@@ -5,9 +5,7 @@ import com.example.travelwise.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,16 @@ public class DepartmentController {
     @GetMapping
     public ResponseEntity<List<DepartmentDTO>> getAllDepartments() {
         return new ResponseEntity<>(departmentService.getAllDepartments(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<DepartmentDTO> createDepartment(@RequestBody DepartmentDTO departmentDTO) {
+        return new ResponseEntity<>(departmentService.createDepartment(departmentDTO), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<DepartmentDTO> deleteDepartment(@PathVariable long id) {
+        departmentService.deleteDepartment(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- TravelWise-backend/src/main/java/com/example/travelwise/service/DepartmentService.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/service/DepartmentService.java
@@ -1,7 +1,9 @@
 package com.example.travelwise.service;
 
 import com.example.travelwise.dto.DepartmentDTO;
+import com.example.travelwise.entity.Department;
 import com.example.travelwise.mapper.DepartmentMapper;
+import com.example.travelwise.repository.CategoryRepository;
 import com.example.travelwise.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,14 +14,26 @@ import java.util.List;
 public class DepartmentService {
     private final DepartmentRepository departmentRepository;
     private final DepartmentMapper departmentMapper;
+    private final CategoryRepository categoryRepository;
 
     @Autowired
-    public DepartmentService(DepartmentRepository departmentRepository, DepartmentMapper departmentMapper) {
+    public DepartmentService(DepartmentRepository departmentRepository, DepartmentMapper departmentMapper, CategoryRepository categoryRepository) {
         this.departmentRepository = departmentRepository;
         this.departmentMapper = departmentMapper;
+        this.categoryRepository = categoryRepository;
     }
 
     public List<DepartmentDTO> getAllDepartments() {
         return departmentRepository.findAll().stream().map(departmentMapper::mapToDTO).toList();
+    }
+
+    public DepartmentDTO createDepartment(DepartmentDTO departmentDTO) {
+        Department newDepartment = departmentRepository.save(departmentMapper.mapToEntity(departmentDTO));
+        return departmentMapper.mapToDTO(newDepartment);
+    }
+
+    public void deleteDepartment(Long id) {
+        categoryRepository.deleteByDepartmentId(id);
+        departmentRepository.deleteById(id);
     }
 }
