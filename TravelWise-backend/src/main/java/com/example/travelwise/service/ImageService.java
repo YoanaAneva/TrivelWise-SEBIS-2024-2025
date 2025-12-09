70c1b5972caf8737d53b3e234a4ec8a0000d3343
Yoana.Aneva <ioana.aneva@gmail.com>
2025-12-09 18:08:09 +0200

    Add profile

--- TravelWise-backend/src/main/java/com/example/travelwise/service/ImageService.java
+++ TravelWise-backend/src/main/java/com/example/travelwise/service/ImageService.java
@@ -7,6 +7,7 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.example.travelwise.repository.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,6 +16,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 @Service
+@Profile("aws")
 public class ImageService {
     private final ImageRepository imageRepository;
     @Value("${application.bucket.name}")
